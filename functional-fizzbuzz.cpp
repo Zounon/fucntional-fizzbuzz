0637f1f14693f3fd02d7dd1606647fbd88ed0f76
Zounon <46566620+Zounon@users.noreply.github.com>
2023-06-06 15:55:08 -0700

    Add files via upload

---
+++ functional-fizzbuzz.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+      /////////////////////////////////////////////
+      ///// Do not edit this block ////////////////
+
+      // number divisible by 3 and 4? print 'FizzBuzz'
+      if (print_fizzbuzz(i))
+				printf("FizzBuzz\t");              
+      
+      // number divisible by 3? print 'Fizz'
+      else if (print_fizz(i))
+        printf("Fizz\t");
+
+      // number divisible by 4, print 'Buzz' 
+      else if (print_buzz(i))
+        printf("Buzz\t");
+
+      // not divisable by 3 or 4? print the number           
+      else
+        printf("%d\t", i);
+
+      ///// Do not edit this block ////////////////
+      /////////////////////////////////////////////
